1e1dd0edcd3116bf1654c65dffefc69413706326
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-15 09:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	date1 = new Date(date1);
+	date2 = new Date(date2);
+	const diff = date2 - date1;
+    const days = Math.ceil(diff/(1000*60*60*24));
+	return days;
 };
 
 // Do not change the code below.
